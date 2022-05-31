7d02ba3e389416120d3065f08701c301a0862728
unknown <kravecandrew@gmail.com>
2022-05-31 11:49:48 +0300

    sae

--- src/car.router/car.controller.ts
+++ src/car.router/car.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { ICar } from './car.interface';
 import { CarService } from './car.service';
 
-@Controller()
+@Controller('cars')
 export class CarController {
   constructor(private readonly carService: CarService) {}
 
