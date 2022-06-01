97ffd24c8f5e8a08f005f1d07138be7ab1d2efcf
unknown <kravecandrew@gmail.com>
2022-06-01 11:36:08 +0300

    save

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: {
       origin: '*',
-      credentials: false,
+      credentials: true,
     },
   });
 
