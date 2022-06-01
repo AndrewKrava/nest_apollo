a351e5a3417ff2cd691f83debd08ae533bfd0897
unknown <kravecandrew@gmail.com>
2022-06-01 11:31:33 +0300

    save

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: true,
+    cors: {
+      origin: '*',
+      credentials: false,
+    },
   });
 
   // TODO modify port
