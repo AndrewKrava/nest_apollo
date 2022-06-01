2aabbe2c702be670bdfc3c319072c61bcd40698e
unknown <kravecandrew@gmail.com>
2022-06-01 11:21:51 +0300

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { CarModule } from './car.router/car.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      cors: true,
+      //   cors: true,
       playground: false,
       introspection: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
 
   // TODO modify port
   const port = process.env.PORT || 3000;
